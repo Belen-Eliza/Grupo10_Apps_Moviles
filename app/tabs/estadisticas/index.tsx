8bbf884e5e0df9b23b3f824d971e14425ac728e3
Belen-Eliza <beluella1@gmail.com>
2024-11-09 17:03:22 -0300

    filtrar por fechas

---
+++ app/tabs/estadisticas/index.tsx
@@ -0,0 +1,92 @@
+import { useUserContext } from "@/context/UserContext";
+import { useState, useEffect } from "react";
+import { Text, View, Pressable, Dimensions,Modal } from "react-native";
+import { estilos,colores } from "@/components/global_styles";
+import React from 'react';
+import { LineChart, PieChart } from "react-native-chart-kit";
+import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
+
+type Category ={id :number, name: string,description: string}
+type Gasto ={ id: number, monto: number, cant_cuotas:number, fecha: Date, category: Category}
+
+export default function Estadisticas (){
+    const context = useUserContext();
+    const [datos,setData] =useState<Gasto[]>([{id:0,monto:0,cant_cuotas:0,fecha: new Date(),category:{id:0,name:"",description:""}}]);
+    const [fecha_desde,setFechaDesde]=useState(new Date(0)); //desde 1970 hasta hoy
+    const [fecha_hasta,setFechaHasta]= useState(new Date())
+    const [modalVisible,setModalVisible] = useState(false);
+
+    useEffect(()=>{
+        (async ()=>{
+            const fechas = {fecha_desde:fecha_desde,fecha_hasta:fecha_hasta}
+            fetch(`${process.env.EXPO_PUBLIC_DATABASE_URL}/gastos/por_fecha/${context.id}`,{
+              method:'GET',
+              headers:{"Content-Type":"application/json"},
+              body:JSON.stringify(fechas)})
+            .then(rsp =>rsp.json())
+            .then(info =>setData(info))
+            console.log(datos)
+        })();
+    },[context.id,fecha_desde,fecha_hasta])
+
+    const data = {
+        labels: datos.map(g=>new Date(g.fecha).toDateString()),
+        datasets: [
+          {
+            data: datos.map(g=>g.monto),
+            color: (opacity = 1) => `rgba(163, 230, 219, ${opacity})`, // optional
+            strokeWidth: 6 // optional
+          }
+        ],
+        legend: ["Gastos"] 
+      };
+    const openDatePicker = ()=>{
+      setModalVisible(true);
+    }
+    const closeDatePicker= ()=>{
+      setModalVisible(false)
+    }
+    const onChangeDesde=(event:DateTimePickerEvent, selectedDate:Date|undefined) => {
+      let currentDate = new Date(0);
+      if (selectedDate!=undefined) currentDate=selectedDate
+      setFechaDesde(currentDate);
+    };
+    const onChangeHasta=(event:DateTimePickerEvent, selectedDate:Date|undefined) => {
+      let currentDate = new Date();
+      if (selectedDate!=undefined) currentDate=selectedDate
+      setFechaHasta(currentDate);
+    };
+    return (<>
+        <View style={estilos.mainView}>
+          <Pressable style={[estilos.tarjeta,estilos.centrado]} onPress={openDatePicker}><Text >Filtrar por fecha</Text></Pressable>
+          <LineChart
+            data={data}
+            width={Dimensions.get("window").width}
+            height={Dimensions.get("window").height*0.7}
+            chartConfig={chartConfig}
+          />
+        </View>
+        <Modal animationType="slide" transparent={false} visible={modalVisible}>
+        <View style={[estilos.mainView,estilos.centrado]}>
+          <Text style={estilos.titulo}>Desde:</Text>
+          <DateTimePicker style={estilos.margen} value={fecha_desde} onChange={onChangeDesde} mode="date" />
+          <Text style={estilos.titulo}>Hasta:</Text>
+          <DateTimePicker style={estilos.margen} onChange={onChangeHasta} value={fecha_hasta} mode="date" />
+          <Pressable style={[estilos.tarjeta,estilos.centrado,colores.botones]} onPress={closeDatePicker}><Text >Confirmar</Text></Pressable>
+        </View>
+        
+        </Modal>
+        </>
+    )
+}
+
+const chartConfig = {
+    backgroundGradientFrom: "#004040",
+    backgroundGradientFromOpacity: 4,
+    backgroundGradientTo: "#005b5b",
+    backgroundGradientToOpacity: 5,
+    color: (opacity = 1) => `rgba(26, 255, 120, ${opacity})`,
+    strokeWidth: 6, // optional, default 3
+    barPercentage: 0.8,
+    useShadowColorFromDataset: false // optional
+  };
--- app/tabs/nuevo/gasto.tsx
+++ app/tabs/nuevo/gasto.tsx
@@ -1,28 +1,36 @@
 import {Pressable, Text, TextInput, View } from "react-native";
 import{estilos,colores} from "@/components/global_styles"
-import { useState, useEffect } from "react";
-
+import { useState,useEffect } from "react";
+import { useUserContext } from "@/context/UserContext"; 
 import DropDownPicker from 'react-native-dropdown-picker';
 import { router } from "expo-router";
 
 type Categoria= {
-  cod: number, descripcion: string
+  id: number, descripcion: string,name:string
 }
 type Gasto ={
-  monto: Number,  categoria_id: number, cant_cuotas: number
+  monto: Number,  category_id: number, cant_cuotas: number,user_id:number
 }
-const todasCategorias = [{value: 1,label:"Comida"},{value: 2,label:"Entretenimiento"}, {value: 3,label:"Hogar"}] /*Conectar despues con DB */
+
 
 export default function Gasto() {
-  const inicial: Gasto = {monto: 0,categoria_id:0,cant_cuotas:1};
+  const inicial: Gasto = {monto: 0,category_id:0,cant_cuotas:1,user_id:0};
   const [gasto,setGasto]=useState(inicial);
   const [openPicker,setOpen] = useState(false);
   const [cat,setCat] =useState(0);
+  const context = useUserContext();
   
+  const [categorias,setCategorias]=useState<Categoria[]>([{id: 1,name:"Comida",descripcion:""}])
   useEffect(()=>{
-    fetch("")
-  })
-  
+    async ()=>{
+      fetch(`${process.env.EXPO_PUBLIC_DATABASE_URL}/categorias/de_gastos`,{
+        method:'GET',
+        headers:{"Content-Type":"application/json"}})
+      .then(rsp =>rsp.json())
+      .then(info =>setCategorias(info))
+    }
+
+  },[context])
   
   const handler_Amount=(input:string)=>{
     let aux=Number(input.replace(",","."));
@@ -45,11 +53,23 @@ export default function Gasto() {
     
   }
 
-  const confirmar= ()=>{
-    gasto.categoria_id=cat;
+  const confirmar= async ()=>{
+    gasto.category_id=cat;
+    gasto.user_id= context.id;
     /*subir info a DB*/
-    alert("Info: Monto="+gasto.monto+", cuotas="+gasto.cant_cuotas+", categoria="+gasto.categoria_id); /*temporal: chequear que se cargó bien*/
-    router.navigate({pathname:"/nuevo",params:{}}) /*volver a inicio para que se actualice el saldo total??*/
+    try {
+    const rsp = await fetch(`${process.env.EXPO_PUBLIC_DATABASE_URL}/gastos/`,{
+      method:'POST',
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify(gasto)})
+
+    if (!rsp.ok){
+      throw new Error
+    }
+    router.navigate({pathname:"/tabs",params:{}})}
+    catch (e){
+      alert(e)
+    }
   } 
     
   return (
@@ -62,7 +82,7 @@ export default function Gasto() {
       <TextInput style={[estilos.textInput,estilos.margen]}  keyboardType="numbers-and-punctuation" onChangeText={handler_Cuotas}  placeholder='Ingresar cuotas'></TextInput>
       
       <Text style={estilos.subtitulo}>Categoría</Text> 
-      <DropDownPicker style={[{maxWidth:"60%"},estilos.textInput,estilos.margen]} open={openPicker} value={cat} items={todasCategorias} setOpen={setOpen} setValue={setCat} />
+      <DropDownPicker style={[{maxWidth:"60%"},estilos.textInput,estilos.margen]} open={openPicker} value={cat} items={categorias.map(e=>{return {cod:e.id,label:e.name}})} setOpen={setOpen} setValue={setCat} />
 
       <Pressable onPress={confirmar} style={[estilos.tarjeta, estilos.centrado,colores.botones, {maxHeight:50}]}><Text style={estilos.subtitulo}>Confirmar</Text></Pressable>
     </View>
