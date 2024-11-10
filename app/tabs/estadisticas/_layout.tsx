2d1a448efddf14dff6f3f4592a4afb14126d0d39
Belen-Eliza <beluella1@gmail.com>
2024-11-10 15:56:39 -0300

    cambios de estructura

--- app/tabs/estadisticas/_layout.tsx
+++ app/tabs/estadisticas/_layout.tsx
@@ -5,6 +5,8 @@ export default function Layout(){
     return(
         <Stack>
             <Stack.Screen name='index' options={{title: "Estadisticas", headerShown: false}}/>
+            <Stack.Screen name='gastos_por_fecha' options={{title: "Estadisticas", headerShown: false}}/>
+            <Stack.Screen name='gastos_por_categoria' options={{title: "Estadisticas", headerShown: false}}/>
             
         </Stack>
     )
---
+++ app/tabs/estadisticas/gastos_por_categoria.tsx
@@ -0,0 +1,13 @@
+import { Pressable, Text, View, StyleSheet } from "react-native";
+import{estilos,colores} from "@/components/global_styles"
+import { Link } from "expo-router";
+
+export default function Gastos_por_Categoria() {
+
+  return (
+    <View style={[{flex: 1},estilos.centrado]} >
+      <Text>Hola</Text>
+      
+    </View>
+  );
+}
