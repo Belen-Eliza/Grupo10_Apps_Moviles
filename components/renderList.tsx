1245f0f2dae9db29649cf26a4228a3c01b04ef6e
Belen-Eliza <beluella1@gmail.com>
2024-11-21 19:28:47 -0300

    mostrar fecha en ingresos

--- components/renderList.tsx
+++ components/renderList.tsx
@@ -1,10 +1,9 @@
-import { View, Pressable, Text, StyleSheet } from "react-native";
+import { Pressable, Text } from "react-native";
 import { estilos,colores } from "@/components/global_styles";
 import { Category } from "./tipos";
 
-//type Category ={id :number, name: string,description: string}
 type Gasto ={ id: number, monto: number, cant_cuotas:number, fecha: Date, category: Category}
-type Ingreso = {id:number,monto: number,description: string,category: Category}
+type Ingreso = {id:number,monto: number,description: string,category: Category,fecha: Date}
 type Presupuesto ={id: number, descripcion: string,montoTotal: number, fecha_objetivo: Date}
 
 function renderGasto ( item : Gasto,callback:Function)  {
@@ -19,8 +18,9 @@ function renderGasto ( item : Gasto,callback:Function)  {
 
 function renderIngreso ( item : Ingreso,callback:Function)  {
     return (
-        <Pressable onPress={()=>callback(item)} style={[estilos.list_element,estilos.margen,estilos.centrado]}>
-        <Text style={[estilos.subtitulo,{alignSelf:"flex-start"}]}>{item.category.name}</Text>
+      <Pressable onPress={()=>callback(item)} style={[estilos.list_element,estilos.margen,estilos.centrado]}>
+        <Text style={{alignSelf:"flex-start",fontSize:10,color:"#909090"}}> {new Date(item.fecha).toDateString()}</Text>
+        <Text style={[estilos.subtitulo]}>{item.category.name}</Text>
         <Text style={{fontSize:20,color:"#909090"}}>{item.description}</Text>
         <Text >Monto: ${item.monto}</Text>
       </Pressable>)
