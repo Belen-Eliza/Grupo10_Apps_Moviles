e88d1f5211b5099323f7d267f9fd86bc757f8330
agusoteroo <agusotero26122002@gmail.com>
2024-12-16 16:35:13 -0300

    aa

--- app/tabs/nuevo/gasto.tsx
+++ app/tabs/nuevo/gasto.tsx
@@ -1,18 +1,28 @@
-import { Pressable, Text, TextInput, View ,Keyboard, TouchableWithoutFeedback, Dimensions,KeyboardAvoidingView,Platform} from "react-native";
+import {
+  Pressable,
+  Text,
+  TextInput,
+  View,
+  Keyboard,
+  TouchableWithoutFeedback,
+  Dimensions,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import { estilos, colores } from "@/components/global_styles";
 import { useEffect, useState } from "react";
 import { CategoryPicker } from "@/components/CategoryPicker";
-import { useUserContext } from "@/context/UserContext"; 
+import { useUserContext } from "@/context/UserContext";
 import { router } from "expo-router";
-import { error_alert, success_alert} from '@/components/my_alert';
-import Toast from 'react-native-toast-message';
+import { error_alert, success_alert } from "@/components/my_alert";
+import Toast from "react-native-toast-message";
 import { Dismiss_keyboard } from "@/components/botones";
 
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
   withSpring,
-} from 'react-native-reanimated';
+} from "react-native-reanimated";
 import { LoadingCircle } from "@/components/loading";
 
 type Gasto = {
@@ -22,22 +32,33 @@ type Gasto = {
   user_id: number;
 };
 
-function es_valido(gasto:Gasto){
-  return gasto.cant_cuotas!=0 && gasto.category_id!=0 && gasto.monto!=0
+function es_valido(gasto: Gasto) {
+  return gasto.cant_cuotas != 0 && gasto.category_id != 0 && gasto.monto != 0;
 }
 export default function Gasto() {
-  const inicial: Gasto = { monto: 0, category_id: 0, cant_cuotas: 0, user_id: 0 };
+  const inicial: Gasto = {
+    monto: 0,
+    category_id: 0,
+    cant_cuotas: 0,
+    user_id: 0,
+  };
   const [gasto, setGasto] = useState(inicial);
   const [openPicker, setOpen] = useState(false);
   const [cat, setCat] = useState(0);
   const context = useUserContext();
   const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
-  const [keyboardHeight,setKeyboardHeight] = useState(300);
-  const [isFetching,setFetching] = useState(false);
+  const [keyboardHeight, setKeyboardHeight] = useState(300);
+  const [isFetching, setFetching] = useState(false);
 
   useEffect(() => {
-    const showSubscription = Keyboard.addListener('keyboardDidShow', handleKeyboardShow);
-    const hideSubscription = Keyboard.addListener('keyboardDidHide', handleKeyboardHide);
+    const showSubscription = Keyboard.addListener(
+      "keyboardDidShow",
+      handleKeyboardShow
+    );
+    const hideSubscription = Keyboard.addListener(
+      "keyboardDidHide",
+      handleKeyboardHide
+    );
 
     return () => {
       showSubscription.remove();
@@ -46,18 +67,18 @@ export default function Gasto() {
 
   const handleKeyboardShow = (event: any) => {
     setIsKeyboardVisible(true);
-    setKeyboardHeight(event.endCoordinates.height)
+    setKeyboardHeight(event.endCoordinates.height);
   };
 
   const handleKeyboardHide = (event: any) => {
     setIsKeyboardVisible(false);
   };
-  
+
   const handler_Amount = (input: string) => {
     let aux = Number(input.replace(",", "."));
     if (Number.isNaN(aux)) error_alert("El valor ingresado debe ser un número");
     else {
-      setGasto(pre => {
+      setGasto((pre) => {
         pre.monto = aux;
         return pre;
       });
@@ -66,7 +87,7 @@ export default function Gasto() {
 
   const handler_Cuotas = (input: string) => {
     let aux = Number(input);
-    setGasto(pre => {
+    setGasto((pre) => {
       pre.cant_cuotas = aux;
       return pre;
     });
@@ -76,30 +97,30 @@ export default function Gasto() {
     gasto.category_id = cat;
     gasto.user_id = context.id;
 
-    
-    if (!es_valido(gasto)) error_alert("Complete todos los campos para continuar");
+    if (!es_valido(gasto))
+      error_alert("Complete todos los campos para continuar");
     else {
       fetch(`${process.env.EXPO_PUBLIC_DATABASE_URL}/gastos/`, {
-        method: 'POST',
+        method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(gasto)
+        body: JSON.stringify(gasto),
       })
-      .then(v=>{
-        setFetching(false);
-        context.actualizar_info(context.id);
-        router.back();
-        setTimeout(()=>success_alert("Gasto creado correctamente"),200)
-      })
-      .catch(e=>{
-        setFetching(false);
-        error_alert(String(e));
-        console.log(e)
-      })
-     
+        .then((v) => {
+          setFetching(false);
+          context.actualizar_info(context.id);
+          router.back();
+          setTimeout(() => success_alert("Gasto creado correctamente"), 200);
+        })
+        .catch((e) => {
+          setFetching(false);
+          error_alert(String(e));
+          console.log(e);
+        });
     }
   };
 
   const scale = useSharedValue(1);
+  const scaleCancel = useSharedValue(1);
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
@@ -108,57 +129,83 @@ export default function Gasto() {
   });
 
   const handlePressIn = () => {
-    scale.value = withSpring(1.1, { damping: 5 }); 
+    scale.value = withSpring(1.1, { damping: 5 });
   };
 
   const handlePressOut = () => {
-    scale.value = withSpring(1, { damping: 5 }); 
+    scale.value = withSpring(1, { damping: 5 });
+  };
+  const animatedStyleCancel = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scaleCancel.value }],
+    };
+  });
+  const handlePressInCancel = () => {
+    scaleCancel.value = withSpring(1.1, { damping: 5 });
+  };
+  const handlePressOutCancel = () => {
+    scaleCancel.value = withSpring(1, { damping: 5 });
   };
 
   return (
-    
-      <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={estilos.flex1}>
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-    <View style={[{ flex: 1 }, estilos.centrado]}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={estilos.flex1}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <View style={[{ flex: 1 }, estilos.centrado]}>
+          {isKeyboardVisible && (
+            <Dismiss_keyboard
+              setVisible={setIsKeyboardVisible}
+              pos_y={Dimensions.get("screen").height - keyboardHeight - 150}
+            />
+          )}
 
-    {isKeyboardVisible && <Dismiss_keyboard setVisible={setIsKeyboardVisible} pos_y={Dimensions.get("screen").height-keyboardHeight-150}/>}
+          <Text style={estilos.subtitulo}>Monto</Text>
+          <TextInput
+            style={[estilos.textInput, estilos.margen]}
+            keyboardType="decimal-pad"
+            onChangeText={handler_Amount}
+            placeholder="Ingresar valor"
+          />
 
-      <Text style={estilos.subtitulo}>Monto</Text>
-      <TextInput
-        style={[estilos.textInput, estilos.margen]}
-        keyboardType="decimal-pad"
-        onChangeText={handler_Amount}
-        placeholder='Ingresar valor'
-      />
-      
-      <Text style={estilos.subtitulo}>Cuotas</Text>
-      <TextInput
-        style={[estilos.textInput, estilos.margen]}
-        keyboardType="number-pad"
-        onChangeText={handler_Cuotas}
-        placeholder='Ingresar cuotas'
-      />
-      
-      <Text style={estilos.subtitulo}>Categoría</Text> 
-      <CategoryPicker openPicker={openPicker} setOpen={setOpen} selected_cat_id={cat} set_cat_id={setCat}></CategoryPicker>
-      
+          <Text style={estilos.subtitulo}>Cuotas</Text>
+          <TextInput
+            style={[estilos.textInput, estilos.margen]}
+            keyboardType="number-pad"
+            onChangeText={handler_Cuotas}
+            placeholder="Ingresar cuotas"
+          />
 
-      <Pressable
-        onPressIn={handlePressIn}
-        onPressOut={handlePressOut}
-        onPress={confirmar}
-      >
-        <Animated.View style={[estilos.tarjeta, estilos.centrado, colores.botones, { maxHeight: 50 }, animatedStyle]}>
-          <Text style={estilos.subtitulo}>Confirmar</Text>
-        </Animated.View>
-      </Pressable>
-    </View>
-    
-    </TouchableWithoutFeedback>
-    <Toast/>
+          <Text style={estilos.subtitulo}>Categoría</Text>
+          <CategoryPicker
+            openPicker={openPicker}
+            setOpen={setOpen}
+            selected_cat_id={cat}
+            set_cat_id={setCat}
+          ></CategoryPicker>
+
+          <Pressable
+            onPressIn={handlePressIn}
+            onPressOut={handlePressOut}
+            onPress={confirmar}
+          >
+            <Animated.View style={[estilos.confirmButton, animatedStyle]}>
+              <Text style={estilos.confirmButtonText}>Confirmar</Text>
+            </Animated.View>
+          </Pressable>
+          <Pressable
+            onPressIn={handlePressInCancel}
+            onPressOut={handlePressOutCancel}
+            onPress={() => router.back()}
+          >
+            <Animated.View style={[estilos.cancelButton, animatedStyleCancel]}>
+              <Text style={estilos.cancelButtonText}>Cancelar</Text>
+            </Animated.View>
+          </Pressable>
+        </View>
+      </TouchableWithoutFeedback>
+      <Toast />
     </KeyboardAvoidingView>
-    
   );
 }
--- app/tabs/nuevo/presupuesto.tsx
+++ app/tabs/nuevo/presupuesto.tsx
@@ -1,95 +1,139 @@
-import {Pressable, Text, TextInput, ScrollView, View, Platform, StyleSheet, Keyboard, KeyboardAvoidingView, Dimensions, TouchableWithoutFeedback } from "react-native";
-import{estilos,colores} from "@/components/global_styles"
+import {
+  Pressable,
+  Text,
+  TextInput,
+  ScrollView,
+  View,
+  Platform,
+  StyleSheet,
+  Keyboard,
+  KeyboardAvoidingView,
+  Dimensions,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { estilos, colores } from "@/components/global_styles";
 import { useEffect, useState } from "react";
-import { useUserContext } from "@/context/UserContext"; 
+import { useUserContext } from "@/context/UserContext";
 import { router } from "expo-router";
-import DateTimePicker, { DateTimePickerEvent, DateTimePickerAndroid, AndroidNativeProps } from '@react-native-community/datetimepicker';
-import Toast from 'react-native-toast-message';
+import DateTimePicker, {
+  DateTimePickerEvent,
+  DateTimePickerAndroid,
+  AndroidNativeProps,
+} from "@react-native-community/datetimepicker";
+import Toast from "react-native-toast-message";
 import { Dismiss_keyboard } from "@/components/botones";
-import {success_alert,error_alert} from '@/components/my_alert';
+import { success_alert, error_alert } from "@/components/my_alert";
 
 import Animated, {
-    useAnimatedStyle,
-    useSharedValue,
-    withSpring,
-  } from 'react-native-reanimated';
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 
-type Presupuesto = { descripcion: string, montoTotal : number,cant_cuotas : number, 
-                    fecha_objetivo: string, total_acumulado: number,user_id: number}
-function es_valido(presupuesto :Presupuesto){
-    return presupuesto.cant_cuotas!=0 && presupuesto.descripcion.length!=0 && presupuesto.montoTotal!=0 
+type Presupuesto = {
+  descripcion: string;
+  montoTotal: number;
+  cant_cuotas: number;
+  fecha_objetivo: string;
+  total_acumulado: number;
+  user_id: number;
+};
+function es_valido(presupuesto: Presupuesto) {
+  return (
+    presupuesto.cant_cuotas != 0 &&
+    presupuesto.descripcion.length != 0 &&
+    presupuesto.montoTotal != 0
+  );
 }
 
 export default function Presupuesto() {
-    const context =useUserContext();
-    const [presupuesto,setPresupuesto] =useState<Presupuesto>({descripcion:"",montoTotal:0,cant_cuotas:0,fecha_objetivo: "",total_acumulado:0,user_id:context.id});
-    const [fecha,setFecha]= useState(new Date());
-    const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
-    const [keyboardHeight,setKeyboardHeight] = useState(300);
+  const context = useUserContext();
+  const [presupuesto, setPresupuesto] = useState<Presupuesto>({
+    descripcion: "",
+    montoTotal: 0,
+    cant_cuotas: 0,
+    fecha_objetivo: "",
+    total_acumulado: 0,
+    user_id: context.id,
+  });
+  const [fecha, setFecha] = useState(new Date());
+  const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
+  const [keyboardHeight, setKeyboardHeight] = useState(300);
 
-    useEffect(() => {
-          const showSubscription = Keyboard.addListener('keyboardDidShow', handleKeyboardShow);
-          const hideSubscription = Keyboard.addListener('keyboardDidHide', handleKeyboardHide);
-      
-          return () => {
-            showSubscription.remove();
-          };
-        }, []);
-      
-        const handleKeyboardShow = (event: any) => {
-          setIsKeyboardVisible(true);
-          setKeyboardHeight(event.endCoordinates.height)
-        };
-      
-        const handleKeyboardHide = (event: any) => {
-          setIsKeyboardVisible(false);
-        };
+  useEffect(() => {
+    const showSubscription = Keyboard.addListener(
+      "keyboardDidShow",
+      handleKeyboardShow
+    );
+    const hideSubscription = Keyboard.addListener(
+      "keyboardDidHide",
+      handleKeyboardHide
+    );
 
-    const handler_descripcion = (input:string)=>{
-        setPresupuesto(pre=>{
-            pre.descripcion=input;
-            return pre
-        })
-    }
-    const handler_monto = (input:string)=>{
-        let aux=Number(input.replace(",","."));
-        if( Number.isNaN(aux)){
-        error_alert("El valor ingresado debe ser un número");
-        } else {
-            setPresupuesto(pre=>{
-                pre.montoTotal=aux;
-                return pre
-            })}
-    }
-    const handler_cuotas = (input:string)=>{
-        let aux=Number(input.replace(",","."));
-        if( Number.isNaN(aux)){
-        error_alert("El valor ingresado debe ser un número");
-        } else {
-            setPresupuesto(pre=>{
-                pre.cant_cuotas=aux;
-                return pre
-            })}
-    }
-    const onChangeDate=(event:DateTimePickerEvent, selectedDate:Date|undefined) => {
-        let currentDate = new Date(0);
-        if (selectedDate!=undefined) currentDate=selectedDate
-        setFecha(currentDate);
-      };
-      const showMode = (currentMode: AndroidNativeProps['mode']) => {
-        DateTimePickerAndroid.open({
-          value: fecha,
-          onChange:onChangeDate,
-          mode: currentMode,
-          is24Hour: false,
-          minimumDate:new Date()
-        });
-      };
-    const showDatepicker = () => {
-        showMode("date");
+    return () => {
+      showSubscription.remove();
     };
+  }, []);
 
-    const scale = useSharedValue(1);
+  const handleKeyboardShow = (event: any) => {
+    setIsKeyboardVisible(true);
+    setKeyboardHeight(event.endCoordinates.height);
+  };
+
+  const handleKeyboardHide = (event: any) => {
+    setIsKeyboardVisible(false);
+  };
+
+  const handler_descripcion = (input: string) => {
+    setPresupuesto((pre) => {
+      pre.descripcion = input;
+      return pre;
+    });
+  };
+  const handler_monto = (input: string) => {
+    let aux = Number(input.replace(",", "."));
+    if (Number.isNaN(aux)) {
+      error_alert("El valor ingresado debe ser un número");
+    } else {
+      setPresupuesto((pre) => {
+        pre.montoTotal = aux;
+        return pre;
+      });
+    }
+  };
+  const handler_cuotas = (input: string) => {
+    let aux = Number(input.replace(",", "."));
+    if (Number.isNaN(aux)) {
+      error_alert("El valor ingresado debe ser un número");
+    } else {
+      setPresupuesto((pre) => {
+        pre.cant_cuotas = aux;
+        return pre;
+      });
+    }
+  };
+  const onChangeDate = (
+    event: DateTimePickerEvent,
+    selectedDate: Date | undefined
+  ) => {
+    let currentDate = new Date(0);
+    if (selectedDate != undefined) currentDate = selectedDate;
+    setFecha(currentDate);
+  };
+  const showMode = (currentMode: AndroidNativeProps["mode"]) => {
+    DateTimePickerAndroid.open({
+      value: fecha,
+      onChange: onChangeDate,
+      mode: currentMode,
+      is24Hour: false,
+      minimumDate: new Date(),
+    });
+  };
+  const showDatepicker = () => {
+    showMode("date");
+  };
+
+  const scale = useSharedValue(1);
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
@@ -98,102 +142,166 @@ export default function Presupuesto() {
   });
 
   const handlePressIn = () => {
-    scale.value = withSpring(1.1, { damping: 5 }); 
+    scale.value = withSpring(1.1, { damping: 5 });
   };
 
   const handlePressOut = () => {
-    scale.value = withSpring(1, { damping: 5 }); 
+    scale.value = withSpring(1, { damping: 5 });
   };
 
-    const confirmar = async ()=>{
-        presupuesto.fecha_objetivo=fecha.toISOString()
-        presupuesto.user_id=context.id;
-        presupuesto.total_acumulado=0;
-        if (!es_valido(presupuesto) || fecha<=(new Date())){
-            error_alert("Complete los espacios en blanco o proporcione una fecha objetivo válida");
-        }
-        else {
-            try {
-                const rsp = await fetch(`${process.env.EXPO_PUBLIC_DATABASE_URL}/presupuestos/`,{
-                    method:'POST',
-                    headers:{"Content-Type":"application/json"},
-                    body:JSON.stringify(presupuesto)})
-            
-                if (!rsp.ok){
-                    throw new Error
-                }
-                
-                router.back();
-                setTimeout(()=>success_alert("Presupuesto creado correctamente"),200);}
-            catch (e){
-                error_alert(String(e));
-                console.log(e)
-            } 
-        }
-         
+  const confirmar = async () => {
+    presupuesto.fecha_objetivo = fecha.toISOString();
+    presupuesto.user_id = context.id;
+    presupuesto.total_acumulado = 0;
+
+    if (!es_valido(presupuesto) || fecha <= new Date()) {
+      error_alert(
+        "Complete los espacios en blanco o proporcione una fecha objetivo válida"
+      );
+      return;
     }
 
+    try {
+      // Crear presupuesto
+      const rsp = await fetch(
+        `${process.env.EXPO_PUBLIC_DATABASE_URL}/presupuestos/`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(presupuesto),
+        }
+      );
 
-    return (
-        
+      if (!rsp.ok) {
+        throw new Error("Error al crear el presupuesto");
+      }
+
+      const presupuestoCreado = await rsp.json(); // Obtener el presupuesto creado
+
+      // Crear categoría asociada al presupuesto
+      const nuevaCategoria = {
+        nombre: presupuesto.descripcion, // Usa la descripción del presupuesto como nombre de la categoría
+        user_id: context.id,
+        presupuesto_id: presupuestoCreado.id, // Asociar al presupuesto creado
+      };
+
+      const rspCategoria = await fetch(
+        `${process.env.EXPO_PUBLIC_DATABASE_URL}/categorias/`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(nuevaCategoria),
+        }
+      );
+
+      if (!rspCategoria.ok) {
+        throw new Error("Error al crear la categoría asociada");
+      }
+
+      // Si todo está bien, redirigir y mostrar éxito
+      router.back();
+      setTimeout(
+        () => success_alert("Presupuesto y categoría creados correctamente"),
+        200
+      );
+    } catch (e) {
+      error_alert(String(e));
+      console.log(e);
+    }
+  };
+
+  return (
     <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={estilos.flex1}>
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={estilos.flex1}
+    >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-        <View style={[estilos.mainView,{alignItems:"center"}]}>
-        {isKeyboardVisible && <Dismiss_keyboard setVisible={setIsKeyboardVisible} pos_y={Dimensions.get("screen").height-keyboardHeight-150}/>}
-        <ScrollView contentContainerStyle={[estilos.mainView,{alignItems:"center"}]} automaticallyAdjustKeyboardInsets={true} >
-            
-            <Text style={[estilos.subtitulo,estilos.poco_margen]}>Monto</Text>
-            <TextInput style={[estilos.textInput,estilos.poco_margen]} keyboardType="decimal-pad" onChangeText={handler_monto}  placeholder='Ingresar valor'></TextInput>
-                
+        <View style={[estilos.mainView, { alignItems: "center" }]}>
+          {isKeyboardVisible && (
+            <Dismiss_keyboard
+              setVisible={setIsKeyboardVisible}
+              pos_y={Dimensions.get("screen").height - keyboardHeight - 150}
+            />
+          )}
+          <ScrollView
+            contentContainerStyle={[estilos.mainView, { alignItems: "center" }]}
+            automaticallyAdjustKeyboardInsets={true}
+          >
+            <Text style={[estilos.subtitulo, estilos.poco_margen]}>Monto</Text>
+            <TextInput
+              style={[estilos.textInput, estilos.poco_margen]}
+              keyboardType="decimal-pad"
+              onChangeText={handler_monto}
+              placeholder="Ingresar valor"
+            ></TextInput>
+
             <Text style={estilos.subtitulo}>Cuotas</Text>
-            <TextInput style={[estilos.textInput,estilos.poco_margen]}  keyboardType="number-pad" onChangeText={handler_cuotas}  placeholder='Ingresar cuotas'></TextInput>
+            <TextInput
+              style={[estilos.textInput, estilos.poco_margen]}
+              keyboardType="number-pad"
+              onChangeText={handler_cuotas}
+              placeholder="Ingresar cuotas"
+            ></TextInput>
 
             <Text style={estilos.subtitulo}>Descripción</Text>
-            <TextInput style={[estilos.textInput,estilos.poco_margen]} keyboardType="default" onChangeText={handler_descripcion}></TextInput>
+            <TextInput
+              style={[estilos.textInput, estilos.poco_margen]}
+              keyboardType="default"
+              onChangeText={handler_descripcion}
+            ></TextInput>
 
             <Text style={estilos.subtitulo}>Fecha objetivo:</Text>
-            {Platform.OS==='android' ?
-                <View style={styles.androidDateTime}>
+            {Platform.OS === "android" ? (
+              <View style={styles.androidDateTime}>
                 <Pressable onPress={showDatepicker}>
-                    <Text style={estilos.show_date}>
-                        {fecha.toLocaleDateString([], {
-                            weekday: "short",
-                            year: "numeric",
-                            month: "short",
-                            day: "2-digit",
-                        })}
-                    </Text>
+                  <Text style={estilos.show_date}>
+                    {fecha.toLocaleDateString([], {
+                      weekday: "short",
+                      year: "numeric",
+                      month: "short",
+                      day: "2-digit",
+                    })}
+                  </Text>
                 </Pressable>
-                </View> 
-                :
-                <DateTimePicker style={estilos.margen} value={fecha} onChange={onChangeDate} mode="date" minimumDate={new Date()}/>
-            }
-            
+              </View>
+            ) : (
+              <DateTimePicker
+                style={estilos.margen}
+                value={fecha}
+                onChange={onChangeDate}
+                mode="date"
+                minimumDate={new Date()}
+              />
+            )}
+
             <Pressable
-                onPressIn={handlePressIn}
-                onPressOut={handlePressOut}
-                onPress={confirmar}
+              onPressIn={handlePressIn}
+              onPressOut={handlePressOut}
+              onPress={confirmar}
             >
-                <Animated.View style={[estilos.tarjeta, estilos.centrado, colores.botones, { maxHeight: 50 }, animatedStyle]}>
+              <Animated.View
+                style={[
+                  estilos.tarjeta,
+                  estilos.centrado,
+                  colores.botones,
+                  { maxHeight: 50 },
+                  animatedStyle,
+                ]}
+              >
                 <Text style={estilos.subtitulo}>Confirmar</Text>
-                </Animated.View>
+              </Animated.View>
             </Pressable>
-        </ScrollView>
+          </ScrollView>
         </View>
-         
-    </TouchableWithoutFeedback>
-    <Toast/>
+      </TouchableWithoutFeedback>
+      <Toast />
     </KeyboardAvoidingView>
-    
   );
 }
 
-
 const styles = StyleSheet.create({
-    androidDateTime: {
-      flexDirection: "row",
-      justifyContent: "space-around",
-    }
-  });
+  androidDateTime: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+});
